775489483955baea0c902cbcdca8e94fc2e35786
Daniel <alteonrene@gmail.com>
2023-08-31 19:48:43 -0400

    Removed cursive at top left of webpage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,11 +33,6 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h20
     px-4 text-white bg-black fixed">
-        
-        
-        <div>
-            <h1 className="text-5xl font-signature ml-2 mt-3">Daniel</h1>
-        </div>
 
 
         <ul className="hidden md:flex">
