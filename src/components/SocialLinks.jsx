3375f100f9f64d1e36b69ee22322f8277c65bd2a
Daniel <alteonrene@gmail.com>
2023-10-11 20:58:33 -0400

    Added resume to file directory

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                     Resume <BsFillPersonLinesFill size={30}/>
                 </>
             ),
-            href: "/resume.pdf",
+            href: "/DevopsEngineerResume.pdf",
             style: "rounded-br-md",
             download: true
         }
@@ -54,10 +54,10 @@ const SocialLinks = () => {
         <ul>
             {links.map(({id, child, href, style, download}) => (
                 <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 " + style}>
-                    <a href={href} 
-                    className="flex justify-between items-center w-full text-white" 
-                    download={download} 
-                    target="_blank" 
+                    <a href={href}
+                    className="flex justify-between items-center w-full text-white"
+                    download={download}
+                    target="_blank"
                     rel="noreferrer">
                     {child}
                     </a>
